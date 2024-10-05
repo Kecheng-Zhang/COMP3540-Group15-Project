e057443619346069a6697c56794da54b32bbf143
“Mengyao <u7465786@anu.edu.au>
2024-10-06 05:08:11 +1100

    Tutorial fixed - Mengyao

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,26 +6,36 @@ using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
 {
-
     public TextMeshProUGUI tutorialText;
-
+    public TextMeshProUGUI encourageText;
     public int movementThreshold;
     private int movementCounter;
-
     private int currentTutorialStage;
-
+    private int previousTutorialStage; 
     public TextMeshProUGUI statusText;
     private GameObject player;
     private PlayerControl playerControl;
+    private CanvasGroup tutorialCanvasGroup;
 
     // Start is called before the first frame update
     void Start()
     {
         currentTutorialStage = 0;
+        previousTutorialStage = -1; 
         movementCounter = 0;
         player = GameObject.Find("Player");
         playerControl = player.GetComponent<PlayerControl>();
         statusText.gameObject.SetActive(false);
+
+        tutorialCanvasGroup = tutorialText.GetComponent<CanvasGroup>();
+        if (tutorialCanvasGroup == null)
+        {
+            tutorialCanvasGroup = tutorialText.gameObject.AddComponent<CanvasGroup>();
+        }
+
+        tutorialCanvasGroup.alpha = 0;
+
+        StartCoroutine(FadeInTutorialText());
     }
 
     void FixedUpdate()
@@ -34,78 +43,95 @@ public class TutorialManager : MonoBehaviour
         setStatus();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Press Tab to show the status of the player
-        if (Input.GetKeyDown(KeyCode.Tab))
+        if (currentTutorialStage != previousTutorialStage)
         {
-            statusText.gameObject.SetActive(true);
-        }
-        // Release Tab to hide the status of the player
-        else if (Input.GetKeyUp(KeyCode.Tab))
-        {
-            statusText.gameObject.SetActive(false);
+            previousTutorialStage = currentTutorialStage;
+            StartCoroutine(FadeInTutorialText()); 
         }
 
-        // Check if the player has moved enough to progress to the next stage
-        if (movementCounter >= movementThreshold)
-        {
-            currentTutorialStage++;
-            movementCounter = 0;
-        }
+        UpdateTutorialText();
 
-        // Display the appropriate tutorial text based on the current stage
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
         if (currentTutorialStage == 0)
         {
-            tutorialText.text = "Welcome to the tutorial! Press 'WASD' to move around.";
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A)
-                || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
             {
                 movementCounter++;
+                if (movementCounter >= movementThreshold)
+                {
+                     StartCoroutine(ShowEncouragementAndProceed("Great!"));
+                    movementCounter = 0;
+                }
             }
         }
         else if (currentTutorialStage == 1)
         {
-            tutorialText.text = "Good! Now move the 'MOUSE' to aim and 'LEFT CLICK' to shoot.";
             if (Input.GetMouseButtonDown(0))
             {
-                currentTutorialStage++;
-            }
-        } else if (currentTutorialStage == 2)
-        {
-            tutorialText.text = "Great! The green dot is the sprint indicator," +
-                "the skill is charging when it is transparent. Now press 'SHIFT' to sprint.";
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                currentTutorialStage++;
-            }
-        } else if (currentTutorialStage == 3)
-        {
-            tutorialText.text = "The yellow bar is the EN (Energy) indicator," +
-                "when it is full, you can activate invincible state by pressing 'SPACE'.";
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                currentTutorialStage++;
-            }
-        } else if (currentTutorialStage == 4)
-        {
-            tutorialText.text = "Also, you can check you status by pressing 'TAB'.";
-            if (Input.GetKeyDown(KeyCode.Tab))
-            {
-                currentTutorialStage++;
-            } else if (Input.GetKeyUp(KeyCode.Tab))
-            {
-            }
+                StartCoroutine(ShowEncouragementAndProceed("Nice Try!"));
         }
-        else if (currentTutorialStage == 5)
-        {
-            tutorialText.text = "Great! You have completed the tutorial. Press 'ESC' to exit.";
-            if (Input.GetKeyDown(KeyCode.Escape))
+        }
+         else if (currentTutorialStage == 2 && Input.GetKeyDown(KeyCode.LeftShift))
             {
-                // Exit the tutorial
-                SceneManager.LoadScene("GameScene");
+                StartCoroutine(ShowEncouragementAndProceed("Perfect!"));
             }
+            else if (currentTutorialStage == 3 && Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(ShowEncouragementAndProceed("Well Done!"));
+            }
+            else if (currentTutorialStage == 4)
+            {
+                if (Input.GetKeyDown(KeyCode.Tab))
+                {
+                    statusText.gameObject.SetActive(true);
+                }else if (Input.GetKeyUp(KeyCode.Tab))
+                    {
+                        statusText.gameObject.SetActive(false);
+                    }
+            
+
+                if (Input.GetKeyDown(KeyCode.Tab))
+                {
+                    StartCoroutine(ShowEncouragementAndProceed("Excellent!"));
+                }
+            }
+        else if (currentTutorialStage == 5 && Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+
+
+
+
+    void UpdateTutorialText()
+    {
+        switch (currentTutorialStage)
+        {
+            case 0:
+                tutorialText.text = "Welcome to the tutorial! Press 'WASD' to move around.";
+                break;
+            case 1:
+                tutorialText.text = "Good! Now move the 'MOUSE' to aim and 'LEFT CLICK' to shoot.";
+                break;
+            case 2:
+                tutorialText.text = "Great! The green dot is the sprint indicator, the skill is charging when it is transparent. Now press 'SHIFT' to sprint.";
+                break;
+            case 3:
+                tutorialText.text = "The yellow bar is the EN (Energy) indicator. When it is full, you can activate invincible state by pressing 'SPACE'.";
+                break;
+            case 4:
+                tutorialText.text = "Also, you can check your status by pressing 'TAB'.";
+                break;
+            case 5:
+                tutorialText.text = "Great! You have completed the tutorial. Press 'ESC' to exit.";
+                break;
         }
     }
 
@@ -127,4 +153,61 @@ public class TutorialManager : MonoBehaviour
         statusText.text += "Speed: " + playerControl.speedLimit + "\n";
         statusText.text += "Sprint CD: " + playerControl.sprintCoolTime + "/s\n";
     }
-}   
+
+    IEnumerator FadeInTutorialText()
+    {
+        float duration = 1.0f; 
+        float elapsedTime = 0f;
+
+        tutorialCanvasGroup.alpha = 0;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            tutorialCanvasGroup.alpha = Mathf.Lerp(0, 1, elapsedTime / duration);
+            yield return null; 
+        }
+
+        tutorialCanvasGroup.alpha = 1;
+    }
+IEnumerator ShowEncouragementAndProceed(string message)
+{
+    CanvasGroup encourageCanvasGroup = encourageText.GetComponent<CanvasGroup>();
+        if (encourageCanvasGroup == null)
+        {
+            encourageCanvasGroup = encourageText.gameObject.AddComponent<CanvasGroup>();
+        }
+
+        encourageText.text = message;
+
+        encourageCanvasGroup.alpha = 0;
+
+        float fadeInDuration = 0.8f; 
+        float fadeOutDelay = 0.8f;  
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeInDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                encourageCanvasGroup.alpha = Mathf.Lerp(0, 1, elapsedTime / fadeInDuration);
+                yield return null;
+            }
+
+        yield return new WaitForSeconds(fadeOutDelay);
+
+        elapsedTime = 0f;
+        float fadeOutDuration = 0.8f; 
+
+        while (elapsedTime < fadeOutDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                encourageCanvasGroup.alpha = Mathf.Lerp(1, 0, elapsedTime / fadeOutDuration);
+                yield return null;
+            }
+
+        encourageCanvasGroup.alpha = 0;
+
+        currentTutorialStage++;
+    }
+
+}
